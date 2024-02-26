6f8f6af8a910fcdebc2d53ec9e25649aa9975feb
OscarMesh <okerekeifeanyi65@gmail.com>
2024-02-26 06:11:42 +0100

    init app and clean template

---
+++ src/app/modal.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { Platform, StyleSheet } from 'react-native';
+
+import { Text, View } from '@/src/components/Themed';
+
+export default function ModalScreen() {
+  return (
+    <View style={styles.container}>
+    
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  
+  },
+
+});
